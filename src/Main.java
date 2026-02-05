e60488dcca150f8447c48e4362ec29813740ab5d
German <ruedapertuzg@gmail.com>
2026-02-05 11:22:57 -0500

    logic backbone

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import Model.Operation;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Operation op=new Operation("1+2*10-2/2");
+
+        System.out.println(op.getResult());
+    }
+}
---
+++ src/Model/Operation.java
@@ -0,0 +1,70 @@
+package Model;
+
+
+import java.util.Arrays;
+import java.util.ArrayList;
+
+public class Operation {
+    private String input;
+    private String[] operations={"*","/","+","-"};
+    private ArrayList<Float> numbers=new ArrayList<>();
+    private ArrayList<String> operators=new ArrayList<>();
+
+    public Operation(String input){
+        this.input=input;
+    }
+
+    public float getResult(){
+
+        String[] splited=this.input.split("");
+        int lastOperator=-1;
+        for (int charac=0; charac< splited.length; charac++){
+            if (Arrays.asList(operations).contains(splited[charac])){
+                if (charac==0){
+
+                }else {
+                    numbers.add(Float.parseFloat(input.substring(lastOperator+1, charac)));
+                    operators.add(splited[charac]);
+                    lastOperator=charac;
+                }
+            }
+        }
+        numbers.add(Float.parseFloat(input.substring(lastOperator+1)));
+        System.out.println(numbers);
+        System.out.println(operators);
+        float result=0;
+        for (int k=0; k<operators.size(); k++){
+            result=operate(numbers.get(k), numbers.get(k+1), operators.get(k));
+
+            numbers.set(k+1, result);
+        }
+        return result;
+
+    }
+
+    public float operate(float n1, float n2, String op){
+        return switch (op) {
+            case "*" -> n1 * n2;
+            case "/" -> n1 / n2;
+            case "+" -> n1 + n2;
+            case "-" -> n1 - n2;
+            default -> 0;
+        };
+    }
+
+    public ArrayList<Float> getNumbers() {
+        return numbers;
+    }
+
+    public void setNumbers(ArrayList<Float> numbers) {
+        this.numbers = numbers;
+    }
+
+    public ArrayList<String> getOperators() {
+        return operators;
+    }
+
+    public void setOperators(ArrayList<String> operators) {
+        this.operators = operators;
+    }
+}
