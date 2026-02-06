9e2acd9aa58625cc8c77ba73020b94a34e3a2f5d
German <ruedapertuzg@gmail.com>
2026-02-06 14:19:07 -0500

    adding parenthesis grouping feature

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,7 @@ import Model.Operation;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Operation op=new Operation("6+9*9-45/3");
-
-        System.out.println(op.getResult());
+        Operation op=new Operation("6+(9*9)-(45/3)-2");
+        System.out.println(op.parenthesisGroups());
     }
 }
--- src/Model/Operation.java
+++ src/Model/Operation.java
@@ -14,12 +14,54 @@ public class Operation {
         this.input=input;
     }
 
-    public float getResult(){
+    public float getResult(String input){
+        ArrayList<String> expression=spliter(input, operations);
 
-        String[] splited=this.input.split("");
+        float result=0;
+        for (int m=0; m<expression.size(); m++){
+            if (expression.get(m).equals("*") || expression.get(m).equals("/")){
+                result=operate(Float.parseFloat(expression.get(m-1)), Float.parseFloat(expression.get(m+1)), expression.get(m));
+                expression.remove(m-1);
+                expression.remove(m-1);
+                expression.set(m-1, String.valueOf(result));
+                m=0;
+            }
+        }
+        System.out.println(expression);
+        for (int n=0; n<expression.size(); n++){
+            if (expression.get(n).equals("+") || expression.get(n).equals("-")){
+                result=operate(Float.parseFloat(expression.get(n-1)), Float.parseFloat(expression.get(n+1)), expression.get(n));
+                expression.remove(n-1);
+                expression.remove(n-1);
+                expression.set(n-1, String.valueOf(result));
+                n=0;
+            }
+        }
+
+        System.out.println(expression);
+
+
+        return result;
+
+    }
+
+    public float operate(float n1, float n2, String op){
+        return switch (op) {
+            case "*" -> n1 * n2;
+            case "/" -> n1 / n2;
+            case "+" -> n1 + n2;
+            case "-" -> n1 - n2;
+            default -> 0;
+        };
+    }
+
+    public ArrayList<String> spliter(String input, String[] characters){
+        String[] splited=input.split("");
+        ArrayList<String> operators=new ArrayList<>();
+        ArrayList<Float> numbers=new ArrayList<>();
         int lastOperator=-1;
         for (int charac=0; charac< splited.length; charac++){
-            if (Arrays.asList(operations).contains(splited[charac])){
+            if (Arrays.asList(characters).contains(splited[charac])){
                 if (charac==0){
 
                 }else {
@@ -32,7 +74,7 @@ public class Operation {
         numbers.add(Float.parseFloat(input.substring(lastOperator+1)));
         System.out.println(numbers);
         System.out.println(operators);
-        float result=0;
+
         //result=operate(numbers.get(k), numbers.get(k+1), operators.get(k));
         //numbers.set(k+1, result);
         ArrayList<String> expression=new ArrayList<>();
@@ -48,39 +90,33 @@ public class Operation {
             }
 
         }
-        for (int m=0; m<expression.size(); m++){
-            if (expression.get(m).equals("*") || expression.get(m).equals("/")){
-                result=operate(Float.parseFloat(expression.get(m-1)), Float.parseFloat(expression.get(m+1)), expression.get(m));
-                expression.remove(m-1);
-                expression.remove(m-1);
-                expression.set(m-1, String.valueOf(result));
-                m=0;
-            }
-        }
-        for (int n=0; n<expression.size(); n++){
-            if (expression.get(n).equals("+") || expression.get(n).equals("-")){
-                result=operate(Float.parseFloat(expression.get(n-1)), Float.parseFloat(expression.get(n+1)), expression.get(n));
-                expression.remove(n-1);
-                expression.remove(n-1);
-                expression.set(n-1, String.valueOf(result));
-            }
-        }
-
-        System.out.println(expression);
-
-        result=operate(Float.parseFloat(expression.get(0)), Float.parseFloat(expression.get(2)), expression.get(1));
-        return result;
-
+        return expression;
     }
 
-    public float operate(float n1, float n2, String op){
-        return switch (op) {
-            case "*" -> n1 * n2;
-            case "/" -> n1 / n2;
-            case "+" -> n1 + n2;
-            case "-" -> n1 - n2;
-            default -> 0;
-        };
+    public ArrayList<String> parenthesisGroups(){
+
+        ArrayList<String> expression=new ArrayList<>();
+        String[] neutral1={"/","*"};
+        String[] neutral2={"+","-"};
+        String[] splited=input.split("");
+        int lastOpen=0;
+
+        for (int charac=0; charac< splited.length; charac++){
+            if (splited[charac].equals("(")){
+                lastOpen=charac;
+            } else if (splited[charac].equals(")")) {
+
+                expression.add(input.substring(lastOpen+1, charac));
+                expression.add(String.valueOf(getResult(input.substring(lastOpen+1, charac))));
+                if(lastOpen!=0){
+                    if (!Arrays.asList(operations).contains(splited[lastOpen-1]) || splited[charac].equals("(")){
+
+                    }
+                }
+            }
+
+        }
+        return expression;
     }
 
     public ArrayList<Float> getNumbers() {
