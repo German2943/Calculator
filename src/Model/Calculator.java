80414afe7654a0016e8d9564af46789e8c9b4ab9
German <ruedapertuzg@gmail.com>
2026-02-27 18:36:15 -0500

    Shunting Yard discarded

--- src/Model/Calculator.java
+++ src/Model/Calculator.java
@@ -1,8 +1,6 @@
 package Model;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.HashMap;
+import java.util.*;
 
 public class Calculator {
     HashMap<String, Operator> pairs=new HashMap<>();
@@ -21,11 +19,8 @@ public class Calculator {
 
 
     public String Calculate(String input){
-        double result;
+        double result=0;
 
-            String postCollision=collisionManagement(input);
-
-            result = evaluatePostFix(inFixToPostFix(postCollision));
 
 
         return String.valueOf(result);
@@ -57,82 +52,6 @@ public class Calculator {
 
 
 
-    public Deque<String> inFixToPostFix(String input){
-        Deque<String> output= new ArrayDeque<>();
-        Deque<String> stack=new ArrayDeque<>();
-        String[] split =input.split("");
-        for (String c: split){
-            if (Character.isDigit(c.charAt(0)) || c.equals(".")){
-                output.addLast(c);
-                
-            } else if (isOperator(c)) {
-                while (!stack.isEmpty() && !stack.getLast().equals("(") &&  isOperator(stack.getLast()) &&
-                        (pairs.get(stack.getLast()).getPrecedence()>pairs.get(c).getPrecedence()
-                || pairs.get(stack.getLast()).getPrecedence()==pairs.get(c).getPrecedence() &&
-                                pairs.get(c).associativity==Associativity.LEFT)){
-
-
-
-                    output.addLast(stack.getLast());
-                    stack.removeLast();
-
-                }
-                stack.addLast(c);
-            } else if (c.equals("(")) {
-                stack.addLast(c);
-            } else if (c.equals(")")) {
-                while(!stack.isEmpty() && !stack.getLast().equals("(")){
-                    output.addLast(stack.getLast());
-                    stack.removeLast();
-                }
-                if (!stack.isEmpty()) stack.removeLast();
-            }
-        }
-
-        while (!stack.isEmpty()){
-            output.addLast(stack.getLast());
-            stack.removeLast();
-        }
-
-
-
-
-        return output;
-    }
-
-
-    public double evaluatePostFix(Deque<String> input){
-        Deque<String> stack=new ArrayDeque<>();
-
-        while (!input.isEmpty()){
-            if (!isOperator(input.getFirst())){
-                stack.addLast(input.getFirst());
-                input.removeFirst();
-            }else {
-                double b=Double.parseDouble(stack.getLast());
-                stack.removeLast();
-                double a=Double.parseDouble(stack.getLast());
-                stack.removeLast();
-                double result=operate(a, b, input.getFirst());
-                stack.addLast(String.valueOf(result));
-                input.removeFirst();
-            }
-
-
-        }
-
-        return Double.parseDouble(stack.getFirst());
-
-    }
-
-
-
-
-
-
-
-
-
 
     public String collisions(String e1, String e2){
         String c=e1+e2;
@@ -149,33 +68,6 @@ public class Calculator {
 
 
 
-
-    public String collisionManagement(String input){
-        String[] expression=input.split("(?<=[+\\-])|(?=[+\\-])");
-
-        StringBuilder output=new StringBuilder();
-
-
-
-
-        for (int i=0; i<expression.length-1; i++){
-
-            if ((expression[i].equals("+") || expression[i].equals("-")) && (expression[i+1].equals("+") || expression[i+1].equals("-"))){
-                expression[i+1]=collisions(expression[i], expression[i+1]);
-                continue;
-
-            }
-
-            output.append(expression[i]);
-            if (i+1==expression.length-1){
-                output.append(expression[i+1]);
-            }
-
-        }
-
-
-        return output.toString();
-    }
 
 
 }
